4360bc13b0cdedac1efe2ca4cdfc6725d77639d6
justincheng20 <justincheng20@gmail.com>
2020-09-06 23:31:47 -0700

    ok ish solution?

--- script.py
+++ script.py
@@ -4,46 +4,38 @@ import csv
 number_of_indices = int(sys.argv[1])
 index = str(sys.argv[2])
 historical_prices = str(sys.argv[3])
-# print('This message will be displayed on the screen.')
-print("ok" + str(number_of_indices) + index + historical_prices)
 
-indice_count = 1
-# use a dictionary?
-chosen_index_values = [5,3]
+indice_count = 0
+chosen_index_values = []
 selected_index_values = {}
+weighted_values = {}
 
 with open(historical_prices) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        print(row[-1])
-        # print(row[0])
-        print(indice_count)
-        print(number_of_indices)
         if row[0] == 'Symbol':
             continue
-        if indice_count == number_of_indices:
-            continue
         elif row[0] == index:
-            # do something
-            # print("index")
-            chosen_index_values[row[0]].append(row[2])
+            chosen_index_values.append(float(row[2]))
         else:
             if row[0] not in selected_index_values.keys():
-                selected_index_values[row[0]] = []
                 indice_count += 1
-            selected_index_values[row[0]].append(row[2])
-      
+                if indice_count > number_of_indices:
+                    continue
+                selected_index_values[row[0]] = []
+                weighted_values[row[0]] = []
+            selected_index_values[row[0]].append(float(row[2]))
 
-chosen_slope = (chosen_index_values[-1] - chosen_index_values[0])/len(chosen_index_values) 
-print(chosen_index_values)
-print('symbol', 'weight')
-for key in selected_index_values.keys():
-    val = (float(selected_index_values[key][-1]) - float(selected_index_values[key][0]))/len(selected_index_values[key]) 
-    print(key, val/chosen_slope)
+for i in range(0,len(chosen_index_values)):
+    current_val = chosen_index_values[i]/number_of_indices
+    for key in selected_index_values.keys():
+        weighted_value = current_val/(selected_index_values[key][i])
+        weighted_values[key].append(weighted_value)
 
-# original_stdout = sys.stdout # Save a reference to the original standard output
+print('Symbol, Weight')
+for key in weighted_values.keys():
+    sum = 0
+    for num in weighted_values[key]:
+        sum += num
+    print(key+",", sum/len(chosen_index_values))
 
-# with open('filename.txt', 'w') as f:
-#     sys.stdout = f # Change the standard output to the file we created.
-#     print('This message will be written to a file.')
-#     sys.stdout = original_stdout # Reset the standard output to its original value
